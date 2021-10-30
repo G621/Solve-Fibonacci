a69805772d55b851377828f189ad00320c7c2f80
7g <63730344+G621@users.noreply.github.com>
2021-10-31 00:47:48 +0800

    Create Solve.py

---
+++ Solve.py
@@ -0,0 +1,27 @@
+# 矩阵幂
+def matrix_pow(a, n):
+    ret = [[1, 0], [0, 1]]
+    while n > 0:
+        if n & 1:
+            ret = matrix_multiply(ret, a)
+        n >>= 1
+        a = matrix_multiply(a, a)
+    return ret
+
+
+# 矩阵乘法
+def matrix_multiply(a, b):
+    c = [[0, 0], [0, 0]]
+    for i in range(2):
+        for j in range(2):
+            c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j]
+    return c
+
+
+def fib(n):
+    if n < 2:
+        return n
+
+    q = [[1, 1], [1, 0]]
+    res = matrix_pow(q, n - 1)
+    return res[0][0]
